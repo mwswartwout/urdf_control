6dff7cbb54fba34f38f1f7e95381b6bee71b1990
Matthew Swartwout <mwswartwout@gmail.com>
2015-10-07 10:15:28 -0400

    Switches from array to std::vector

--- src/urdf_commander.cpp
+++ src/urdf_commander.cpp
@@ -1,6 +1,7 @@
 #include <ros/ros.h>
 #include <sensor_msgs/JointState.h>
 #include <math.h>
+#include <vector>
 
 int main(int argc, char **argv) {
     ros::init(argc, argv, "minimal_commander"); // minimal_commander node
@@ -18,17 +19,20 @@ int main(int argc, char **argv) {
     double amplitude2 = pi; // amplitude value for joint2
     double frequency2 = 4; // frequency value for joint2
     int seq = 0;
-
+    std::vector<double> position;
+    std::vector<std::string> name;
     sensor_msgs::JointState output; // message wrapper for sine output
     while (ros::ok()) {
         sine1 = amplitude1 * sin(2*pi*frequency1*t); // Calculate sine1
         sine2 = amplitude2 * sin(2*pi*frequency2*t); // Calculate sine2
         
         output.header.seq = seq;
-        output.name[1] = "joint1";
-        output.name[2] = "joint2";
-        output.position[1] = sine1; // Store sine value in proper message format
-        output.position[2] = sine2;
+        name[0] = "joint1";
+        name[1] = "joint2";
+        output.name = name;
+        position[0] = sine1; // Store sine value in proper message format
+        position[1] = sine2;
+        output.position = position;
         joint_publisher.publish(output); // Publish value to vel_cmd topic
         t += dt; // Increment t by timeset dt
         naptime.sleep();
