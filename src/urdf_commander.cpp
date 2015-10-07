eb5ac38ab06179237ba22fac2a072b5cd23dc45a
Matthew Swartwout <mwswartwout@gmail.com>
2015-10-07 00:11:28 -0400

    Updates to publish JointState instead of Float64

--- src/urdf_commander.cpp
+++ src/urdf_commander.cpp
@@ -5,8 +5,7 @@
 int main(int argc, char **argv) {
     ros::init(argc, argv, "minimal_commander"); // minimal_commander node
     ros::NodeHandle n;
-    ros::Publisher command_publisher = n.advertise<sensor_msgs::JointState>("joint1", 1); // publish to vel_cmd topic
-    ros::Publisher command_publisher = n.advertise<sensor_msgs::JointState>("joint2", 1); // publish to vel_cmd topic
+    ros::Publisher joint_publisher = n.advertise<sensor_msgs::JointState>("joint1", 1); // publish to vel_cmd topic
     ros::Rate naptime(10); // update @ 10hz
 
     double pi = 3.14159; // value of pi
@@ -18,14 +17,19 @@ int main(int argc, char **argv) {
     double frequency1 = 1; // frequency value for joint1
     double amplitude2 = pi; // amplitude value for joint2
     double frequency2 = 4; // frequency value for joint2
- 
-    std_msgs::Float64 output; // message wrapper for sine output
+    int seq = 0;
 
+    sensor_msgs::JointState output; // message wrapper for sine output
     while (ros::ok()) {
         sine1 = amplitude1 * sin(2*pi*frequency1*t); // Calculate sine1
         sine2 = amplitude2 * sin(2*pi*frequency2*t); // Calculate sine2
-        output.data = sine; // Store sine value in proper message format
-        command_publisher.publish(output); // Publish value to vel_cmd topic
+        
+        //output.header.seq = seq;
+        output.name[1] = "joint1";
+        output.name[2] = "joint2";
+        output.position[1] = sine1; // Store sine value in proper message format
+        output.position[2] = sine2;
+        joint_publisher.publish(output); // Publish value to vel_cmd topic
         t += dt; // Increment t by timeset dt
         naptime.sleep();
     }
